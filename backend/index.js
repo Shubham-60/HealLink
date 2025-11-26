8c1314c1007e72f3ee9853203ed1cf4cefab41b7
Shubham-60 <workshubham38@gmail.com>
2025-11-26 18:10:51 +0530

    cors changes

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 // Optional extra cors() (safe but not required)
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
   })
 );
